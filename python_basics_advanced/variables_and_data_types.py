dc6309678ad24cf5c4a0d6dce6c0edd986a79880
Stephen Saunders <saunders.stephen@gmail.com>
2023-11-17 20:58:39 -1000

    Add new file variables_and_data_types.py

---
+++ python_basics_advanced/variables_and_data_types.py
@@ -0,0 +1,13 @@
+
+# TODO: Declare a variable called 'name' and assign it your name as a string
+name = "Stephen"
+
+# TODO: Declare a variable called 'age' and assign it your age as an integer
+age = 18
+
+# TODO: Declare a variable called 'is_student' and assign it a boolean value indicating whether you are a student or not
+# TODO: Declare a variable called 'height' and assign it your height as a float
+# TODO: Declare a variable called 'favorite_fruits' and assign it a list of your favorite fruits
+# TODO: Declare a variable called 'person' and assign it a dictionary with keys 'name', 'age', 'is_student', 'height', and 'favorite_fruits'
+
+# TODO: Print the values of the variables to verify their contents
