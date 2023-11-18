8f1c762158e5c733166ad0fd4ccddfb830f80654
Stephen Saunders <saunders.stephen@gmail.com>
2023-11-17 21:11:45 -1000

    Add variables and data types to python_basics_advanced

--- python_basics_advanced/variables_and_data_types.py
+++ python_basics_advanced/variables_and_data_types.py
@@ -1,13 +1,27 @@
 
-# TODO: Declare a variable called 'name' and assign it your name as a string
+# Declare a variable called 'name' and assign it your name as a string
 name = "Stephen"
 
-# TODO: Declare a variable called 'age' and assign it your age as an integer
+# Declare a variable called 'age' and assign it your age as an integer
 age = 18
 
-# TODO: Declare a variable called 'is_student' and assign it a boolean value indicating whether you are a student or not
-# TODO: Declare a variable called 'height' and assign it your height as a float
-# TODO: Declare a variable called 'favorite_fruits' and assign it a list of your favorite fruits
-# TODO: Declare a variable called 'person' and assign it a dictionary with keys 'name', 'age', 'is_student', 'height', and 'favorite_fruits'
+# Declare a variable called 'is_student' and assign it a boolean value indicating whether you are a student or not
+is_student = True
 
-# TODO: Print the values of the variables to verify their contents
+# Declare a variable called 'height' and assign it your height as a float
+height = 5.07
+
+# Declare a variable called 'favorite_fruits' and assign it a list of your favorite fruits
+favorite_fruits = ["pear", "mango", "coconut"]
+
+# Declare a variable called 'person' and assign it a dictionary with keys 'name', 'age', 'is_student', 'height', and 'favorite_fruits'
+person = {
+    "name": name,
+    "age": age,
+    "is_student": is_student,
+    "height": height,
+    "favorite_fruits": favorite_fruits
+}
+
+# Print the values of the variables to verify their contents
+print(person)
