a2a31dc0c7ca391727cb21785a6c4363c6b7fa8a
Stephen Saunders <saunders.stephen@gmail.com>
2023-11-17 21:20:26 -1000

    Add hometown to person dictionary

--- python_basics_advanced/01_variables_and_data_types.py
+++ python_basics_advanced/01_variables_and_data_types.py
@@ -14,12 +14,16 @@ height = 5.07
 # Declare a variable called 'favorite_fruits' and assign it a list of your favorite fruits
 favorite_fruits = ["pear", "mango", "coconut"]
 
+# Declare a variable called 'hometown' and assign the latitude and longitude of your hometown as a tuple
+hometown = (40.7128, 74.0060)
+
 # Declare a variable called 'person' and assign it a dictionary with keys 'name', 'age', 'is_student', 'height', and 'favorite_fruits'
 person = {
     "name": name,
     "age": age,
     "is_student": is_student,
     "height": height,
+    "hometown": hometown,
     "favorite_fruits": favorite_fruits
 }
 
