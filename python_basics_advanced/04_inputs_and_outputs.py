023f7ec5255e0098d55c5fe292af599dec022084
Stephen Saunders <saunders.stephen@gmail.com>
2023-11-18 19:51:20 -1000

    Add square function to calculate the square of a number

--- python_basics_advanced/04_inputs_and_outputs.py
+++ python_basics_advanced/04_inputs_and_outputs.py
@@ -3,7 +3,14 @@
 name = input("What is your name? ")
 print(f"Hello, {name}!")
 
-# TODO: Write a program that reads a number from the user and prints its square.
+# Write a program that reads a number from the user and prints its square.
+number = int(input("Enter a number: "))
+
+def square(num):
+    return num ** 2
+    
+print(square(number))
+
 # TODO: Write a program that reads two numbers from the user and prints their sum.
 # TODO: Write a program that reads a sentence from the user and prints the number of words in the sentence.
 # TODO: Write a program that reads a list of numbers from the user and prints the maximum and minimum numbers in the list.
