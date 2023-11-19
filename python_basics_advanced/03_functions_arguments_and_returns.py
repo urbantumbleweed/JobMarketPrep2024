63384278b6c383135d02d508b68f3c99d613fa48
Stephen Saunders <saunders.stephen@gmail.com>
2023-11-18 19:13:22 -1000

    Add function making prompts
    Add functions for greeting, adding numbers,
    calculating average, checking if a number is even,
    and finding the maximum number

---
+++ python_basics_advanced/03_functions_arguments_and_returns.py
@@ -0,0 +1,20 @@
+
+# Define a function called "greet" that takes a name as an argument and prints a greeting message
+def greet(name):
+    print(f"Hello, {name}!")
+
+# Call the "greet" function with your name as the argument
+name = input("What is your name? ")
+greet(name)
+
+# TODO: Define a function called "add_numbers" that takes two numbers as arguments and returns their sum
+# TODO: Call the "add_numbers" function with two numbers of your choice and print the result
+
+# TODO: Define a function called "calculate_average" that takes a list of numbers as an argument and returns their average
+# TODO: Call the "calculate_average" function with a list of numbers of your choice and print the result
+
+# TODO: Define a function called "is_even" that takes a number as an argument and returns True if it is even, False otherwise
+# TODO: Call the "is_even" function with a number of your choice and print the result
+
+# TODO: Define a function called "get_max" that takes a list of numbers as an argument and returns the maximum number
+# TODO: Call the "get_max" function with a list of numbers of your choice and print the result
