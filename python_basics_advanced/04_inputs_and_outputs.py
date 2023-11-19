f5c8ae4cdd2f2fa9b140d54e26a9a22ea121d554
Stephen Saunders <saunders.stephen@gmail.com>
2023-11-18 19:53:18 -1000

    Add program to calculate sum of two numbers

--- python_basics_advanced/04_inputs_and_outputs.py
+++ python_basics_advanced/04_inputs_and_outputs.py
@@ -11,7 +11,15 @@ def square(num):
     
 print(square(number))
 
-# TODO: Write a program that reads two numbers from the user and prints their sum.
+# Write a program that reads two numbers from the user and prints their sum.
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+
+def add(num1, num2):
+    return num1 + num2
+
+print(add(number1, number2))
+
 # TODO: Write a program that reads a sentence from the user and prints the number of words in the sentence.
 # TODO: Write a program that reads a list of numbers from the user and prints the maximum and minimum numbers in the list.
 # TODO: Write a program that reads a file name from the user and prints the contents of the file.
