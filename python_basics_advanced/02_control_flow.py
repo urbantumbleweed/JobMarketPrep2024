e194edc7fdc06d7194813bde0b41f40140bd4169
Stephen Saunders <saunders.stephen@gmail.com>
2023-11-18 18:51:34 -1000

    Add control flow logic to determine age category

--- python_basics_advanced/02_control_flow.py
+++ python_basics_advanced/02_control_flow.py
@@ -61,6 +61,13 @@ print(factorial)
 # If the age is less than 18, print "You are a minor"
 # If the age is between 18 and 65, print "You are an adult"
 # If the age is greater than 65, print "You are a senior citizen"
+age = int(input("Enter your age: "))
+if age < 18:
+    print("You are a minor")
+elif age >= 18 and age <= 65:
+    print("You are an adult")
+else:
+    print("You are a senior citizen")
 
 # Write a program that asks the user for a number
 # If the number is divisible by 3, print "Fizz"
