af71b01361ea9d835ddad6f371d4af2498dfa10f
Stephen Saunders <saunders.stephen@gmail.com>
2023-11-18 18:32:09 -1000

    Add control flow exercises

---
+++ python_basics_advanced/02_control_flow.py
@@ -0,0 +1,68 @@
+
+# Control Flow Exercises
+
+# 1. If Statements
+
+# Use an if statement to check if a number is positive or negative
+# If the number is positive, print "The number is positive"
+# If the number is negative, print "The number is negative"
+
+number = 5
+if number > 0:
+    print("The number is positive")
+elif number < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
+
+# 2. For Loops
+
+# Use a for loop to iterate over a list of numbers and print each number
+# The list of numbers is [1, 2, 3, 4, 5]
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    print(number)
+
+# Use a for loop to iterate over a string and print each character
+# The string is "Hello, World!"
+
+# 3. While Loops
+
+# Use a while loop to print numbers from 1 to 5
+# Start with a variable `num` set to 1
+# Increment `num` by 1 in each iteration
+# Stop the loop when `num` is greater than 5
+
+# Use a while loop to find the factorial of a number
+# The factorial of a number is the product of all positive integers less than or equal to that number
+# Start with a variable `num` set to 5
+# Initialize a variable `factorial` with a value of 1
+# Multiply `factorial` by `num` in each iteration
+# Decrement `num` by 1 in each iteration
+# Stop the loop when `num` is less than or equal to 0
+
+# 4. Control Flow in Programs
+
+# Write a program that asks the user for their age
+# If the age is less than 18, print "You are a minor"
+# If the age is between 18 and 65, print "You are an adult"
+# If the age is greater than 65, print "You are a senior citizen"
+
+# Write a program that asks the user for a number
+# If the number is divisible by 3, print "Fizz"
+# If the number is divisible by 5, print "Buzz"
+# If the number is divisible by both 3 and 5, print "FizzBuzz"
+# Otherwise, print the number itself
+
+# Write a program that asks the user for a password
+# If the password is "password123", print "Access granted"
+# Otherwise, print "Access denied"
+
+# Write a program that asks the user for a grade (a number between 0 and 100)
+# If the grade is greater than or equal to 90, print "A"
+# If the grade is between 80 and 89, print "B"
+# If the grade is between 70 and 79, print "C"
+# If the grade is between 60 and 69, print "D"
+# If the grade is less than 60, print "F"
+
+# Feel free to modify the code and add more exercises as you explore control flow in Python!
