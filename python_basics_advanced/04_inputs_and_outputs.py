7f97543c60cd9d7ff2c764d444adf64a9db78dbb
Stephen Saunders <saunders.stephen@gmail.com>
2023-11-18 19:48:17 -1000

    Add input and output programs

---
+++ python_basics_advanced/04_inputs_and_outputs.py
@@ -0,0 +1,9 @@
+
+# TODO: Write a program that prompts the user to enter their name and prints a welcome message.
+# TODO: Write a program that reads a number from the user and prints its square.
+# TODO: Write a program that reads two numbers from the user and prints their sum.
+# TODO: Write a program that reads a sentence from the user and prints the number of words in the sentence.
+# TODO: Write a program that reads a list of numbers from the user and prints the maximum and minimum numbers in the list.
+# TODO: Write a program that reads a file name from the user and prints the contents of the file.
+
+# Your code goes here...
